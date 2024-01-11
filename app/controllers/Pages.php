9fa45306e03dd60958f2bff05b59bce4e44b0e75
Daysi Cruz <daysicruz960@gmail.com>
2024-01-11 12:36:27 -0600

    Update - Loading Views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,9 +4,10 @@ class Pages extends Controller {
         
     }
     public function index() {
-        
+        $data = ['title' => 'Welcome'];
+        $this->view('pages/index', $data);
     }
-    public function about($id) {
-        echo $id;
+    public function about() {
+        $this->view('pages/about');
     }
 }
