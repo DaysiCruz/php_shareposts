deda182913258b47cfef6a63ca978f191dae591f
Daysi Cruz <daysicruz960@gmail.com>
2024-02-08 22:01:08 -0600

    Update - User Session Data & Logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -123,7 +123,7 @@ class User extends Controller {
                 $loggedInUser = $this->userModel->login($data['email'],$data['password']);
                 if ($loggedInUser) {
                     // Create Session
-                    die('SUCCESS');
+                    $this->createUserSession($loggedInUser);
                 } else {
                     $data['password_err'] = 'Password Incorrect';
                     $this->view('users/login',$data);
@@ -145,5 +145,25 @@ class User extends Controller {
             //load view
             $this->view('users/login', $data);
         }
+        }
+        public function createUserSession($user) {
+            $_SESSION['user_id'] = $user->id;
+            $_SESSION['user_email'] = $user->email;
+            $_SESSION['user_name'] = $user->name;
+            redirect('pages/index');
+        }
+        public function logout() {
+            unset($_SESSION['user_id']);
+            unset($_SESSION['user_email']);
+            unset($_SESSION['user_name']);
+            session_destroy();
+            redirect('users/login');
+        }
+        public function isLoggenInd() {
+            if (isset($_SESSION['user_id'])) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
-}
