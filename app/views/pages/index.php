4087d3b851706b833bb1313ff51e9e985165b987
Daysi Cruz <daysicruz960@gmail.com>
2024-03-01 23:47:47 -0600

    Update - Haciendo mas responsivo el navbar

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php' ?>
 <div class="jumbotron jumbotron-fluid text-center mt-5 pt-5">
-    <div class="containe bg-primary-subtle border --bs-success-border-subtle rounded-3">
-    <h1 class="display-2 "><?php echo $data['title']; ?></h1>
+    <div class="container col-lg-8 bg-primary-subtle border --bs-success-border-subtle rounded-3">
+    <h1 class="display-3"><?php echo $data['title']; ?></h1>
     <p class="lead"><?php echo $data['description']; ?></p>
     </div>
 </div>
