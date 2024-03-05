374d45919ef51877551d0af1485b52399760496f
Daysi Cruz <daysicruz960@gmail.com>
2024-03-05 10:12:39 -0600

    Cambiando el protocolo HTTP a HTTPS para hacer nuestro sitio seguro

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
 ////DB params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'us_sharepost');
-define('DB_PASSWORD', 'toor1234');
+define('DB_USER', 'sharepost');
+define('DB_PASSWORD', '1234');
 define('DB_NAME', 'shareposts');
 
 
 //App Root
 define ( 'APPROOT', dirname(dirname(__FILE__))) ;
 //URL Root
-define('URLROOT', 'http://shareposts.com/');
+define('URLROOT', 'https://shareposts.com/');
 // Site name
 define('SETNAME', 'SharePosts');
 //App Version
